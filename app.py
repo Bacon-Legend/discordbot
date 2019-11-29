657fdd586a36e5311619f14dab5a19a25dbe995f
zhixiang zhao <matthewzhaocc@gmail.com>
2019-11-28 17:23:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ async def on_message(message):
         await message.channel.send(rand)
     return 
 
-client.run('NjQ5NDY4MTQ5MzU1MzgwNzQ3.Xd9OZg.j2KIKsh6Yb-NjRPaR_9651hqVM')
+client.run('NjQ5NDY4MTQ5MzU1MzgwNzQ3.XeBzLQ.jKttubjqi61RB5lxYnrlxRlXHBg')
