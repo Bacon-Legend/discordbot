e56483c3a6a71020c7845a7ca67cc59acda7a4bf
zhixiang zhao <matthewzhaocc@gmail.com>
2019-11-28 17:50:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ async def on_message(message):
         rand = random.randint(1,10)
         await message.channel.send(rand)
     return 
-TOKEN = "NjQ5NDY4MTQ5MzU1MzgwNzQ3.XeB4nQ.G3DZjFd4Syclv7O3gIKb08zA5Wc"
+TOKEN = "NjQ5NDY4MTQ5MzU1MzgwNzQ3.XeB5ig.AycIkAEEhX7izRmpUdEaNbb3jKE'
 client.run(TOKEN)
